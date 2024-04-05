21383cebdcd5e2a7872e02fc8d3670c8df86e2a0
Alematri <alessandromatricardi@gmail.com>
2024-04-05 17:33:25 +0200

    Bonus

--- src/org/wishlist/Main.java
+++ src/org/wishlist/Main.java
@@ -1,10 +1,15 @@
 package org.wishlist;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
+    private static final String FILENAME = "wishlist.txt";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Wish> wishList = new ArrayList<>();
@@ -33,12 +38,20 @@ public class Main {
         // Ordina la lista
         Collections.sort(wishList);
 
-        // Stampare
-        System.out.println("Lista dei desideri:");
-        for (Wish wish : wishList) {
-            System.out.println(wish.getName());
-        }
+        // Scrivi la lista su file
+        printWishListToFile(wishList);
 
         scanner.close();
+    }
+
+    private static void printWishListToFile(ArrayList<Wish> wishList) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILENAME))) {
+            for (Wish wish : wishList) {
+                writer.println(wish.getName());
+            }
+            System.out.println("Lista dei desideri salvata su file: " + FILENAME);
+        } catch (IOException e) {
+            System.out.println("Errore durante la scrittura su file: " + e.getMessage());
+        }
     }
 }
